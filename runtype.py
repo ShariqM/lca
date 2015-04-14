77e48945f50fedb734f065a77588e7af11e110f2
Shariq Mobin <Shariq.Mobin@gmail.com>
2015-04-13 18:02:44 -0700

    try mlca

--- runtype.py
+++ runtype.py
@@ -1,12 +1,15 @@
 class RunType():
-    Learning = 1
-    vLearning = 2
-    vReconstruct = 3
+    Learning     = 1
+    vLearning    = 2
+    vmLearning   = 3
+    vReconstruct = 4
 
 def get_RunType_name(rt):
     if rt == RunType.Learning:
         return 'Learning'
     if rt == RunType.vLearning:
         return 'vLearning'
+    if rt == RunType.vmLearning:
+        return 'vmLearning'
     if rt == RunType.vReconstruct:
         return 'vReconstruct'
