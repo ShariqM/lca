001a5b3fbe697866f7857b1370439c226615900f
Shariq Mobin <shariq.mobin@gmail.com>
2015-05-14 17:22:43 -0700

    more act analysis

--- activity_analysis.py
+++ activity_analysis.py
@@ -22,14 +22,37 @@ class Analysis():
 
 
     def over_time(self, coeffs, patch_index):
-        for pi in range(30): #(0,1,2,3):
+        pdb.set_trace()
+        for pi in range(3): #(0,1,2,3):
             for i in coeffs:
                 plt.plot(range(self.nframes), self.activity_log[i, pi,:], label='P_%d_A%d' % (pi, i))
         plt.title("Activity over %d frames for patch %d" % (self.nframes, patch_index))
         #plt.legend()
         plt.show()
 
-    def correlation(self, coeffs, patch_index):
+    def spatial_correlation(self, coeffs, patch_index):
+        if len(coeffs) != 2:
+            raise Exception("Greater than 2 dim not supported.")
+
+        patches = range(self.patches)[:10]
+        x = self.activity_log[coeffs[0], patches, :]
+        y = self.activity_log[coeffs[1], patches, :]
+        #for i in range(self.patches):
+            #pdb.set_trace()
+            #x += self.activity_log[coeffs[0], i, :]
+            #y += self.activity_log[coeffs[1], i, :]
+
+        h = max(np.max(x), np.max(y))
+        plt.plot([-h, h], [0, 0], color='k')
+        plt.plot([0, 0], [-h, h], color='k')
+
+        plt.scatter(x, y, s=1, color='red')
+        plt.xlabel("A%d" % coeffs[0], fontdict={'fontsize':18})
+        plt.ylabel("A%d" % coeffs[1], fontdict={'fontsize':18})
+        plt.axis('equal')
+        plt.show()
+
+    def temporal_correlation(self, coeffs, patch_index):
         if len(coeffs) != 2:
             raise Exception("Greater than 2 dim not supported.")
 
@@ -61,5 +84,5 @@ group = [46,47] # Strong correlatioN
 group = [0,1]
 
 a = Analysis(data)
-#a.over_time(group, patch_index)
-a.correlation(group, patch_index)
+a.over_time(group, patch_index)
+#a.spatial_correlation(group, patch_index)
