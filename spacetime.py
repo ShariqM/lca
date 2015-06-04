35f987943b51e1c322c21d77275c715f4e518103
Shariq Mobin <Shariq.Mobin@gmail.com>
2015-06-03 20:29:09 -0700

    try theano

--- spacetime.py
+++ spacetime.py
@@ -34,8 +34,12 @@ class SpaceTime():
     time_batch_size = 64
     num_trials = 1000
 
-    eta_init = 0.6
+    eta_init = 6.0
     eta_inc  = 100
+
+    citers    = 60
+    coeff_eta = 0.05
+    lambdav   = 0.1
 
     data_name = 'IMAGES_DUCK_SHORT'
     profile = True
@@ -48,6 +52,7 @@ class SpaceTime():
         self.images = self.images[self.data_name]
         (self.imsize, imsize, self.num_images) = np.shape(self.images)
         self.patch_per_dim = int(np.floor(imsize / self.sz))
+        plt.ion()
 
     def load_videos(self):
         imsize, sz, tbs = self.imsize, self.sz, self.time_batch_size
@@ -62,7 +67,10 @@ class SpaceTime():
             VI[:,x,:] = np.reshape(img, (self.patch_dim, tbs), 1)
         return VI
 
-    def normalize_Phi(self, Phi):
+    def normalize_Phi(self, Phi, a):
+        #Phi = np.einsum('pnt,nn->pnt', Phi, np.diag(np.average(np.sum(a ** 2, axis=2), axis=1)))
+        for i in range(self.timepoints):
+            Phi[:,:,i] = np.dot(Phi[:,:,i], np.diag(1/np.sqrt(np.sum(Phi[:,:,i]**2, axis = 0))))
         return Phi
 
     def get_eta(self, trial):
@@ -83,6 +91,7 @@ class SpaceTime():
             I = VI[:,:,t]
             size = min(self.timepoints - 1, t)
             e[:,:,t] = I - np.einsum('pnt,nbt->pb', Phi[:,:,0:size+1], a[:,:,t::-1][:,:,0:size+1])
+            e[:,:,t] = I - tendot('pnt,nbt->pb', Phi[:,:,0:size+1], a[:,:,t::-1][:,:,0:size+1])
         self.profile_print("Error Calc", start)
         return e
 
@@ -99,21 +108,6 @@ class SpaceTime():
     def phi_cot(self, a, e):
         'Correlation over time'
         start = dt.now()
-        x = np.zeros((self.patch_dim, self.neurons, self.timepoints))
-
-        for b in range(self.batch_size):
-            for p in range(self.patch_dim):
-                for n in range(self.neurons):
-                    for tau in range(self.timepoints):
-                        for t in range(self.time_batch_size):
-                            if t - tau < 0:
-                                break
-                            if t+tau >= self.time_batch_size:
-                                break
-                            x[p,n,tau] = a[n,b,t-tau] * e[p,b,t+tau]
-        self.profile_print("dPhi Calc", start)
-
-        start = dt.now()
         result = np.zeros((self.patch_dim, self.neurons, self.timepoints))
         for tau in range(self.timepoints):
             for t in range(self.time_batch_size):
@@ -122,30 +116,7 @@ class SpaceTime():
                 if t+tau >= self.time_batch_size:
                     break
                 result[:,:,tau] = np.einsum('nb,pb->pn', a[:,:,t-tau], e[:,:,t+tau])
-
-        #for tau in range(self.timepoints):
-            #end = self.time_batch_size - tau
-            #end2 = self.time_batch_size
-            #result[:,:,tau] = np.einsum('nbt,pbt->pn', a[:,:,0:end], e[:,:,tau:end2])
-        #if not np.allclose(result, x):
-            #pdb.set_trace()
         self.profile_print("dPhi2 Calc", start)
-
-        #for t in range(self.timepoints):
-            #size = self.time_batch_size
-            #result[:,:,t] = np.einsum('nbt,pbt->pn', a[:,:,t::-1][:,:,0:size+1],
-                                      #e[:,:,t:t+size+1])
-#
-        #for t in range(self.time_batch_size):
-            #size = min(self.timepoints - 1, self.time_batch_size - t - 1)
-            #size = min(size, t)
-            #print 't=%d, size=%d' % (t, size)
-            #try:
-                #result[:,:,t] = np.einsum('nbt,pbt->pn', a[:,:,t::-1][:,:,0:size+1],
-                                          #e[:,:,t:t+size+1])
-            #except Exception as ex:
-                #pdb.set_trace()
-        #self.profile_print("dPhi Calc", start)
         return result
 
     def profile_print(self, msg, start):
@@ -154,40 +125,80 @@ class SpaceTime():
         diff = dt.now() - start
         print '%10s | E=%s' % (msg, diff)
 
+    def sparse_cost(self, a):
+        return (2 * a) / (1 + a ** 2)
+
+    def get_activity(self, a):
+        max_active = self.batch_size * self.time_batch_size * self.neurons
+        ac = np.copy(a)
+        #ac[np.abs(ac) > 0.01] = 1
+        #ac[np.abs(ac) <= 0.00] = 0
+        ac[np.abs(ac) > self.lambdav] = 1
+        ac[np.abs(ac) <= self.lambdav] = 0
+        return 100 * np.sum(ac)/max_active
+
     def train(self):
-        load = False
-        if not load:
-            Phi = np.random.randn(self.patch_dim, self.neurons, self.timepoints)
+        Phi = np.random.randn(self.patch_dim, self.neurons, self.timepoints)
+        if False:
+            Phi[:,:,0] = np.dot(Phi[:,:,0], np.diag(1/np.sqrt(np.sum(Phi[:,:,0]**2, axis = 0))))
+            for i in range(1, self.timepoints):
+                Phi[:,:,i] = Phi[:,:,i-1] + 0.1 * np.random.randn(self.patch_dim, self.neurons)
+                Phi[:,:,i] = np.dot(Phi[:,:,i], np.diag(1/np.sqrt(np.sum(Phi[:,:,i]**2, axis = 0))))
+        else:
             for i in range(self.timepoints):
                 Phi[:,:,i] = np.dot(Phi[:,:,i], np.diag(1/np.sqrt(np.sum(Phi[:,:,i]**2, axis = 0))))
-            np.save('tmp_phi', Phi)
-        else:
-            Phi = np.load('tmp_phi.npy')
-
-        self.coeff_eta = 0.05
-        self.citers = 40
+        self.showbfs(Phi)
 
         for trial in range(self.num_trials):
             VI = self.load_videos()
             a = np.zeros((self.neurons, self.batch_size, self.time_batch_size))
 
-            if not load:
-                for c in range(self.citers):
-                    e = self.error(VI, Phi, a)
-                    print '%d) E=%.3f' % (c, np.sum(np.abs(e)))
-                    da = self.a_cot(Phi, e)
-                    a += self.coeff_eta * da
-                np.save('tmp_a', a)
-            else:
-                a = np.load('tmp_a.npy')
+            for c in range(self.citers):
                 e = self.error(VI, Phi, a)
-                print 'E=%.3f' % (np.sum(np.abs(e)))
+                if c == self.citers or c % 10 == 0:
+                    print '\t%d) E=%.3f Activity=%.2f%%' % \
+                        (c, np.sum(np.abs(e)), self.get_activity(a))
+                da = self.a_cot(Phi, e) - self.lambdav * self.sparse_cost(a)
+                a += self.coeff_eta * da
+
             dPhi = self.phi_cot(a, e)
-            #pdb.set_trace()
             Phi += self.get_eta(trial) * dPhi
             e = self.error(VI, Phi, a)
-            print 'E2=%.3f' % (np.sum(np.abs(e)))
-            Phi = self.normalize_Phi(Phi)
+            print '%d) E1=%.3f' % (trial, np.sum(np.abs(e)))
+            Phi = self.normalize_Phi(Phi, a)
+            e = self.error(VI, Phi, a)
+            print '%d) E2=%.3f' % (trial, np.sum(np.abs(e)))
+            self.showbfs(Phi)
+
+    def showbfs(self, Phi):
+        L = self.patch_dim
+        M = self.neurons
+        sz = self.sz
+        n = floor(sqrt(M)) # sz of one side of the grid of images
+        m = ceil(M/n) # ceil for 1 extra
+        buf = 1
+
+        sz = self.sz
+
+        for t in range(self.timepoints):
+            arr = ones(shape=(buf + n * (sz + buf), buf + m * (sz + buf)))
+            for k in range(M):
+                i = (k % n)
+                j = floor(k/n)
+
+                def index(x):
+                    return buf + x * (sz + buf)
+
+                maxA=max(abs(Phi[:,k,t])) # RESCALE
+                img = reshape(Phi[:,k,t], (sz, sz), order='C').transpose()/maxA
+                arr[index(i):index(i)+sz, index(j):index(j)+sz] = img
+
+            plt.imshow(arr, cmap = cm.binary, interpolation='none')
+            plt.title('Phi @ t=%d' % t)
+            plt.draw()
+            plt.show()
+            time.sleep(0.5)
+
 
 st = SpaceTime()
 st.train()
